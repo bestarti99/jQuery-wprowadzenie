ab5d0a991da6885d9ab216ef852bc5354d98c68a
bestarti99 <praca.uek@outlook.com>
2017-06-24 12:59:45 +0200

    o matko

--- js/script.js
+++ js/script.js
@@ -44,15 +44,15 @@ $(document).ready(function () {
 
     // Metody dodające i usuwające klasy
     $('.trzeci').addClass('czwarty').removeClass('trzeci');
-    
+
     // Metody find i each
     $('#znajdz').find('.find').css({
-        'background-color':'blue',
-        'color':'#fff',
-        'font-size':'30px'
-        
+        'background-color': 'blue',
+        'color': '#fff',
+        'font-size': '30px'
+
     })
-    
+
     // dla wszystlich spanów
     /*$('#znajdz').find('span').css({
         'background-color':'blue',
@@ -60,24 +60,85 @@ $(document).ready(function () {
         'font-size':'30px'
         
     })*/
-    
-    $('#kazdy ul li').each(function(index) {
+
+    $('#kazdy ul li').each(function (index) {
         console.log($(this).text());
         // używamy this bo użycie $('#kazdy ul li') spowodowałoby dołożenie wszystkich klas do klientów
         $(this).addClass('list-item' + index);
     })
 
-        
+
     // Eventy
-    $('#eventy button').each(function(){
+    //Pierwsze rozwiązanie z .each
+    /*$('#eventy button').each(function(){
         $(this).click(function(){
             console.log('Przycisk został wciśnięty');
         })
+    })*/
+
+    clickMouseOver();
+    // showHide();
+    fades();
+    
+    // drugie rozwiązanie z .on, na .on można podpiąć kilka zdarzeń
+
+    // zmiana koloru oi zmianie rozmiaru okna
+    $(window).resize(function(){
+        changeColor();
+    });
+    
+    $(window).scroll(function() {
+        changePosition();
     })
-        
-        
-        
-   
+    
+    function clickMouseOver() {
+        $('#eventy button').on({
+            'click': function () {
+                console.log('Przycisk został wciśnięty');
+            },
 
-
+            'mouseover': function () {
+               console.log('Jestem nad przyciskiem');
+            }
+        })
+    }
 })
+
+function changeColor() {
+    $('#first').css('background-color', 'blue')
+}
+
+function changePosition() {
+    var parent = $('#main-nav');
+    var wysokosc = $('#main-nav').height();
+    // console.log(wysokosc);
+//    console.log($(window).scrollTop());
+    
+    var offset = $(window).scrollTop();
+        
+        if (offset >= wysokosc) {
+            $(parent).css({
+                'position':'fixed',
+                'width':'100%'
+            })
+        } else {
+            $(parent).css('position', 'static');
+        }
+    }
+
+function showHide() {
+    var parent = $('#showHide');
+    
+    $(parent).hide(5000).show(5000);
+}
+
+function fades() {
+    var parent = $('#showHide');
+    
+    $(parent).fadeOut(5000).fadeIn(5000);
+
+    
+
+}
+
+
